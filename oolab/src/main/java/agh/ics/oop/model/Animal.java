6b16b687b109318b32fc2c773ef51d0d7b366c52
Anna Konieczna <annkonieczna@student.agh.edu.pl>
2025-12-27 00:33:36 +0100

    Modify Animal further

--- oolab/src/main/java/agh/ics/oop/model/Animal.java
+++ oolab/src/main/java/agh/ics/oop/model/Animal.java
@@ -7,15 +7,17 @@ public class Animal implements WorldElement {
     private MapDirection direction;
     private int energy;
     private final int[] genome;
-    private int activeGene;
+    private int activeGeneIndex;
 
+    private int plantsEaten = 0;
+    private int age = 0;
 
-    public Animal(Vector2d position,int startEnergy, int[] genome) {
+    public Animal(Vector2d position, int startEnergy, int[] genome) {
         this.position = position;
         this.energy = startEnergy;
         this.genome = genome;
         this.direction = MapDirection.values()[new Random().nextInt(8)];
-        this.activeGene = genome[new Random().nextInt(genome.length)];
+        this.activeGeneIndex = new Random().nextInt(genome.length);
     }
 
 
@@ -36,11 +38,30 @@ public class Animal implements WorldElement {
     public Vector2d getPosition() {
         return position;
     }
-    public void looseEnergy(int amount) {
-        energy-= amount;
+
+    public void loseEnergy(int amount) {
+        energy -= amount;
     }
 
-    
+    public void eatGrass(int amount) {
+        energy += amount;
+        plantsEaten += 1;
+    }
 
+    public int getEnergy() {
+        return energy;
+    }
+
+    public void move(WorldMap map) {
+        int activeGene = genome[activeGeneIndex];
+        direction = direction.rotate(activeGene);
+
+        Vector2d moveVector = direction.toUnitVector();
+//      position =
+
+        activeGeneIndex = (activeGeneIndex + 1) % genome.length;
+        age++;
+
+    }
 
 }
