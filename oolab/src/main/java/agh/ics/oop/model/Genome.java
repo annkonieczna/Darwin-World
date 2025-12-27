7e854b37532166e4a1f6b490d38a0fc9992d90b4
Anna Konieczna <annkonieczna@student.agh.edu.pl>
2025-12-27 02:57:18 +0100

    Reformatted file

--- oolab/src/main/java/agh/ics/oop/model/Genome.java
+++ oolab/src/main/java/agh/ics/oop/model/Genome.java
@@ -29,31 +29,28 @@ public final class Genome {
             lessEnergy = energyA;
         }
 
-        double ratio = (double) moreEnergy /(moreEnergy + lessEnergy);
-        int strongerGeneCount = (int) Math.round(ratio*length);
+        double ratio = (double) moreEnergy / (moreEnergy + lessEnergy);
+        int strongerGeneCount = (int) Math.round(ratio * length);
         boolean isLeft = new Random().nextBoolean();
         int[] child = new int[length];
-        if(isLeft) {
-            System.arraycopy(stronger,0, child,0,strongerGeneCount);
-            System.arraycopy(weaker,strongerGeneCount, child,strongerGeneCount,length-strongerGeneCount);
+        if (isLeft) {
+            System.arraycopy(stronger, 0, child, 0, strongerGeneCount);
+            System.arraycopy(weaker, strongerGeneCount, child, strongerGeneCount, length - strongerGeneCount);
 
         } else {
-            int weakerGeneCount = length-strongerGeneCount;
-            System.arraycopy(weaker,0, child,0,weakerGeneCount);
-            System.arraycopy(stronger,weakerGeneCount,child,weakerGeneCount,strongerGeneCount);
+            int weakerGeneCount = length - strongerGeneCount;
+            System.arraycopy(weaker, 0, child, 0, weakerGeneCount);
+            System.arraycopy(stronger, weakerGeneCount, child, weakerGeneCount, strongerGeneCount);
         }
-        mutate(child,minMutations,maxMutations);
+        mutate(child, minMutations, maxMutations);
         return child;
 
 
     }
 
-    public static void mutate(int[] child,int min,int max) {
+    public static void mutate(int[] child, int min, int max) {
         if (max == 0) return;
     }
-
-
-
 
 
 }
