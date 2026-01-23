bfe21b0d9c12173bf19d87a7aa535bacccfc7093
Anna Konieczna <annkonieczna@student.agh.edu.pl>
2026-01-23 18:45:44 +0100

    Add implementations for methods added in an interface

--- oolab/src/main/java/agh/ics/oop/model/map/EarthMap.java
+++ oolab/src/main/java/agh/ics/oop/model/map/EarthMap.java
@@ -4,15 +4,20 @@ import agh.ics.oop.model.movement.MapDirection;
 import agh.ics.oop.model.movement.Movement;
 import agh.ics.oop.model.movement.Vector2d;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class EarthMap extends AbstractWorldMap {
     private final Boundary bounds;
     private final Random random = new Random();
     private final Boundary jungleBoundary;
+    private final Map<Vector2d, Integer> goodGrassSpawnMap = new HashMap<>();
+    private final Map<Vector2d, Integer> toxicGrassSpawnMap = new HashMap<>();
 
     public EarthMap(int width, int height, Boundary jungleBoundary) {
-        this.bounds = new Boundary(new Vector2d(0,0), new Vector2d(width-1,height-1));
+        this.bounds = new Boundary(new Vector2d(0, 0), new Vector2d(width - 1, height - 1));
         this.jungleBoundary = jungleBoundary;
     }
 
@@ -37,14 +42,13 @@ public class EarthMap extends AbstractWorldMap {
             x = bounds.lowerLeft().getX();
         }
 
-        return new Movement(new Vector2d(x,y), direction);
+        return new Movement(new Vector2d(x, y), direction);
     }
 
     @Override
     public Vector2d randomPositionFromMap() {
-        Vector2d position = new Vector2d(random.nextInt(bounds.lowerLeft().getX(),bounds.upperRight().getX()+1),
-                random.nextInt(bounds.lowerLeft().getY(),bounds.upperRight().getY())+1);
-        return position;
+        return new Vector2d(random.nextInt(bounds.lowerLeft().getX(), bounds.upperRight().getX() + 1),
+                random.nextInt(bounds.lowerLeft().getY(), bounds.upperRight().getY()) + 1);
     }
 
     @Override
@@ -53,11 +57,31 @@ public class EarthMap extends AbstractWorldMap {
     }
 
     @Override
+    public void registerGrowth(Vector2d position, boolean toxic) {
+        if (toxic) {
+            toxicGrassSpawnMap.merge(position, 1, Integer::sum);
+        } else {
+            goodGrassSpawnMap.merge(position, 1, Integer::sum);
+        }
+    }
+
+
+    @Override
     public Boundary getBounds() {
         return bounds;
     }
 
     public Boundary getJungleBoundary() {
         return jungleBoundary;
+    }
+
+    @Override
+    public Map<Vector2d, Integer> getGoodGrassSpawnMap() {
+        return Collections.unmodifiableMap(goodGrassSpawnMap);
+    }
+
+    @Override
+    public Map<Vector2d, Integer> getToxicGrassSpawnMap() {
+        return Collections.unmodifiableMap(toxicGrassSpawnMap);
     }
 }
